6dac728b011590e8acdc4ea9cf127486d4430431
Shawn <sppang1@yahoo.com>
2018-12-10 16:32:36 -0500

    created tests to see if divs are rendering

--- client/src/tests/index.test.js
+++ client/src/tests/index.test.js
@@ -9,8 +9,8 @@ it('renders without crashing', () => {
   shallow(<CreativeCollab />);
 });
 
-it("always renders a div", () => {
+it('always renders a div', () => {
   const wrapper = shallow(<CreativeCollab />);
-  const divs = wrapper.find("div");
+  const divs = wrapper.find('div');
   expect(divs.length).toBeGreaterThan(0);
 });
